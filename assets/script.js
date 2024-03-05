299e4599c86256cf1cc59db100dc41c6b5b14f09
PrestonNguyen2001 <prestonnguyen2001@gmail.com>
2024-03-04 23:07:36 -0500

    change css styling and improved readability

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,16 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {
+menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+});
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
+// Add scroll event listener
+window.addEventListener('scroll', () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 100;
@@ -23,33 +24,31 @@ window.onscroll = () => {
             });
 
             sec.classList.add('show-animate');
-        }
-
-        else {
+        } else {
             sec.classList.remove('show-animate');
         }
     });
-    let header = document.querySelector('header');
 
+    let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 
+    // Close navbar and reset menu icon on scroll
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
-}
+});
 
 // Add event listeners for "Read More" buttons
-let readMoreButtons = document.querySelectorAll('.read-more');
+let readMoreButtons = document.querySelectorAll('#readMoreBtn');
 let additionalTexts = document.querySelectorAll('.additional-text');
 
 readMoreButtons.forEach((button, index) => {
     button.addEventListener('click', (event) => {
-        event.preventDefault(); // Prevent the default action of the anchor tag
+        event.preventDefault(); 
         additionalTexts[index].classList.toggle('show');
-        
+
         // Hide the button when the additional text is shown
         if (additionalTexts[index].classList.contains('show')) {
             button.style.display = 'none';
         }
     });
 });
-
